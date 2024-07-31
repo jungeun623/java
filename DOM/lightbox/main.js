5f75843e90ed3f383150963bd002d73356114804
lee jungeun <jungeun623@gmail.com>
2024-07-31 11:32:14 +0900

    dom 등 함수

---
+++ DOM/lightbox/main.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function () {
+  const pics = this.document.querySelectorAll(".pic");
+  const lightbox = this.document.querySelector("#lightbox");
+  const lightboxImg = this.document.querySelector("#lightboxImage");
+  pics.forEach(function (pic) {
+    // console.log(pic);
+    pic.addEventListener("click", function () {
+      // console.log(this);
+      const bicImg = this.getAttribute("data-src");
+      lightboxImg.setAttribute("src", bicImg);
+      lightbox.style.display = "block";
+    //   클릭하면 
+    });
+  });
+  lightbox.onclick = function () {
+    lightbox.style.display = "none"
+  };
+});
